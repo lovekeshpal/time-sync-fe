b87f034daba78414707ac6907862b4bc2a197f95
lovekeshpal <lovekeshfgr@gmail.com>
2025-01-02 01:10:05 +0530

    login register notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LocalStorageService } from './services/local-storage/local-storage.service';
 
 @Component({
   selector: 'app-root',
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'] // Corrected property name
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   @HostBinding('class') className = '';
+
+  constructor(private localStorageService: LocalStorageService) { }
+
+  ngOnInit() {
+    const savedTheme = this.localStorageService.getItem('theme');
+    if (savedTheme) {
+      this.className = savedTheme;
+      document.documentElement.classList.add(savedTheme);
+    }
+  }
 
   toggleTheme() {
     const darkMode = document.documentElement.classList.toggle('dark');
     this.className = darkMode ? 'dark' : '';
+    this.localStorageService.setItem('theme', this.className);
   }
-
 }
