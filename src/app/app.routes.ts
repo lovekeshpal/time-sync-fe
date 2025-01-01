4ad05c1187942364f8ed0865a51835053304b7d6
lovekeshpal <lovekeshfgr@gmail.com>
2025-01-02 00:57:26 +0530

    login sign up page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'sign-up', component: SignUpComponent },
+    { path: 'login', component: LoginComponent },
+
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3490dc",
+        "primary-dark": "#2779bd",
+        secondary: "#ffed4a",
+        text: "#333",
+        "text-light": "#fff",
+        "bg-light": "#f8fafc",
+        "bg-dark": "#1a202c",
+      },
+    },
   },
   plugins: [],
   darkMode: "class", // Enable dark mode
