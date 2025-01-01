4f2ff64d4cec9819731f390b995e92afd13a7a01
lovekeshpal <lovekeshfgr@gmail.com>
2025-01-02 00:27:09 +0530

    light and dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostBinding } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  @HostBinding('class') className = '';
+
+  toggleTheme() {
+    const darkMode = document.documentElement.classList.toggle('dark');
+    this.className = darkMode ? 'dark' : '';
+  }
+
 }
