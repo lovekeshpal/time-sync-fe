7910ed5272029f4910805926d7d2c6022887cf2b
lovekeshpal <lovekeshfgr@gmail.com>
2025-01-18 21:47:51 +0530

    dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'signup', component: SignUpComponent },
     { path: 'login', component: LoginComponent },
-    
+    { path: 'dashboard', component: DashboardComponent }
 ];
