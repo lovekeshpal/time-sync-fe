93da4fbe21b44eca83959a24f57448caab42f4a3
lovekeshpal <lovekeshfgr@gmail.com>
2025-03-01 23:57:13 +0530

    auth guard changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AuthGuard } from './guards/auth/auth.guard';
+import { CreateTimerComponent } from './components/timer/create-timer/create-timer.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
@@ -11,6 +12,11 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'create-timer',
+    component: CreateTimerComponent,
     canActivate: [AuthGuard],
   },
 ];
